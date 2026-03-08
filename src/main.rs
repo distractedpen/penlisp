7b9ca3dbacb57447307eb477d31e627417468d84
Taylor Noble <zachnoble1993@gmail.com>
2026-03-08 18:26:21 -0400

    simple shell mode and cli flags added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::{self}, fmt::Debug, fs::File, io::{BufReader, Read}};
+use std::{env::{self}, fmt::Debug, fs::{File}, io::{self, BufReader, Read, Write}};
 
 #[derive(Debug)]
 enum Atom {
@@ -10,22 +10,91 @@ enum Atom {
     Division,
 }
 
-
-fn read_file(filename: String) -> std::io::Result<String>
-{ let f = File::open(filename)?;
-    let mut reader = BufReader::new(f);
-
-    let mut data = String::new();
-    reader.read_to_string(&mut data)?;
-
-    Ok(data.trim().to_string())
+struct CliArgs {
+    shell_mode: bool,
+    filename: String,
 }
+
+fn parse_args() -> Result<CliArgs, String> {
+    let raw_args: Vec<String> = env::args().collect();
+    let mut shell_mode = true;
+    let mut filename = String::new();
+
+    let mut index = 1;
+    while index < raw_args.len() {
+
+        match raw_args.get(index) {
+            Some(a) => {
+                match (*a).as_str() {
+                    "--noshell" => {
+                        shell_mode = false;
+                    },
+                    "--file" | "-f" => {
+                        filename = match raw_args.get(index + 1) {
+                            Some(arg) => (*arg).clone(),
+                            None => {
+                                return Err("Missing filename for file flag.".to_string())
+                            }
+                        };
+                        index += 1;
+                    },
+                    _ => {
+                        return Err(format!("Unknown flag: {}", *a));
+                    },
+                }
+            }
+            None => {
+                return Err("How did we get an empty in the args?".to_string());
+            }
+        };
+
+        index += 1;
+    }
+
+    match raw_args.iter().position(|f| f == "-f" || f == "--file") {
+        Some(index) => {
+            if index + 1 > raw_args.len() {
+                return Err("Error: filename not provided.".to_string());
+            }
+            filename = raw_args[index + 1].to_string();
+        },
+        None => {},
+    }
+
+    Ok(CliArgs {
+        shell_mode,
+        filename,
+    })
+
+}
+
+
+fn interpreter(data: String) {
+    // Parsing a singular S expression
+    let tokens: Vec<String> = tokenize(data);
+    print!("{:?}\n", tokens);
+    let input_stack = process_tokens(tokens);
+    print!("{:?}\n", input_stack);
+    let result = process_stack(input_stack);
+    match result {
+        Ok(a) => {
+            match a {
+                Atom::Number(x)  => print!("{}\n", x),
+                Atom::Literal(s) => print!("{}\n", s),
+                _                => print!("{:?}", a),
+            }
+        },
+        Err(s) => {
+            println!("{}", s);
+        }
+    }
+}
+
 
 fn tokenize(data: String) -> Vec<String> {
     let mut tokens: Vec<String> = vec![];
     let chars: Vec<char> = data.chars().collect();
     let mut chars_index = 0;
-
     let mut token = String::new();
     while chars_index < chars.len() {
         match chars[chars_index] {
@@ -169,30 +238,61 @@ fn process_stack(mut input_stack: Vec<Atom>) -> Result<Atom, &'static str> {
     Ok(process_stack.pop().unwrap())
 }
 
+fn read_file(filename: String) -> std::io::Result<String>
+{ let f = File::open(filename)?;
+    let mut reader = BufReader::new(f);
+
+    let mut data = String::new();
+    reader.read_to_string(&mut data)?;
+
+    Ok(data.trim().to_string())
+}
+
+fn shell_mode() -> std::io::Result<()> {
+    let mut input = String::new();
+    let prompt = "> ";
+    print!("Starting Shell mode. Press Ctrl-C to exit.\n");
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush()?;
+        let n = io::stdin().read_line(&mut input)?;
+        if n == 0 {
+            break
+        } else {
+            interpreter(input.clone());
+        }
+        input.clear();
+    }
+
+    Ok(())
+}
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-    let filename = args[1].clone();
-
-    let data = read_file(filename).expect("Failed to Read input file.");
-
-    // Parsing a singular S expression
-    let tokens: Vec<String> = tokenize(data);
-    print!("{:?}\n", tokens);
-    let input_stack = process_tokens(tokens);
-    print!("{:?}\n", input_stack);
-    let result = process_stack(input_stack);
-    match result {
-        Ok(a) => {
-            match a {
-                Atom::Number(x)  => print!("{}\n", x),
-                Atom::Literal(s) => print!("{}\n", s),
-                _                => print!("{:?}", a),
+    let args = match parse_args() {
+        Ok(a) => a,
+        Err(error) => {
+            print!("{}", error);
+            return;
+        }
+    };
+    if args.shell_mode {
+        if !args.filename.is_empty() {
+            let data = read_file(args.filename).expect("Failed to Read input file.");
+            interpreter(data);
+        }
+        match shell_mode() {
+            Ok(_) => {
+                println!("Exiting...");
             }
-        },
-        Err(s) => {
-            println!("{}", s);
+            Err(error) => {
+                println!("{}", error);
+            }
+        }
+    } else {
+        if !args.filename.is_empty() {
+            let data = read_file(args.filename).expect("Failed to Read input file.");
+            interpreter(data);
         }
     }
 }
