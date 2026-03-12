e6a11c3dc23f91a6f87d237ebe5c72c7c40b8e5e
Taylor Noble <zachnoble1993@gmail.com>
2026-03-11 22:21:17 -0400

    this is not using enums, lol

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use std::{env::{self}, fmt::Debug, fs::{File}, io::{self, BufReader, Read, Write}};
+use std::{env::{self}, fs::{File}, io::{BufReader, Read, Write}};
 
-#[derive(Debug)]
-enum Atom {
-    Literal(String),
-    Number(i64),
-    Add,
-    Subtraction,
-    Muliplication,
-    Division,
-}
+mod penlisp;
 
 struct CliArgs {
     shell_mode: bool,
@@ -69,175 +61,6 @@ fn parse_args() -> Result<CliArgs, String> {
 }
 
 
-fn interpreter(data: String) {
-    // Parsing a singular S expression
-    let tokens: Vec<String> = tokenize(data);
-    print!("{:?}\n", tokens);
-    let input_stack = process_tokens(tokens);
-    print!("{:?}\n", input_stack);
-    let result = process_stack(input_stack);
-    match result {
-        Ok(a) => {
-            match a {
-                Atom::Number(x)  => print!("{}\n", x),
-                Atom::Literal(s) => print!("{}\n", s),
-                _                => print!("{:?}", a),
-            }
-        },
-        Err(s) => {
-            println!("{}", s);
-        }
-    }
-}
-
-
-fn tokenize(data: String) -> Vec<String> {
-    let mut tokens: Vec<String> = vec![];
-    let chars: Vec<char> = data.chars().collect();
-    let mut chars_index = 0;
-    let mut token = String::new();
-    while chars_index < chars.len() {
-        match chars[chars_index] {
-            ' ' | '\n' | '\t' => {
-                if !token.is_empty() {
-                    tokens.push(token.clone());
-                    token.clear();
-                }
-            }
-            '(' => {
-                tokens.push(String::from("("));
-            }
-            ')' => {
-                if !token.is_empty() {
-                    tokens.push(token.clone());
-                    token.clear();
-                }
-                tokens.push(String::from(")"));
-            }
-            _ => {
-                token.push(chars[chars_index]);
-            }
-        }
-
-        chars_index += 1;
-    }
-
-    tokens
-}
-
-
-fn process_tokens(tokens: Vec<String>) -> Vec<Atom> {
-    let mut stack: Vec<Atom> = Vec::new();
-    let mut index = 0;
-
-    while index < tokens.len() {
-        let token = &tokens[index];
-
-        match token.as_str() {
-            "(" => {
-                index += 1;
-                let action = tokens.get(index).expect("Malformed S-Expression.").as_str();
-                match action {
-                    "+" => {
-                        stack.push(Atom::Add);
-                    }
-                    "-" => {
-                        stack.push(Atom::Subtraction);
-                    }
-                    "*" => {
-                        stack.push(Atom::Muliplication);
-                    }
-                    "/" => {
-                        stack.push(Atom::Division);
-                    }
-                    _ => {
-                        todo!("Unimplemented action");
-                    }
-                }
-            }
-            ")" => {
-                // TODO:
-                // Use this route to handle live interpreter parsing??
-            }
-            _ => {
-                match token.parse::<i64>() {
-                    Err(_) => stack.push(Atom::Literal(String::from(token))),
-                    Ok(value) => stack.push(Atom::Number(value)),
-                };
-            }
-        }
-
-        index += 1;
-    }
-
-    stack
-}
-
-
-fn process_stack(mut input_stack: Vec<Atom>) -> Result<Atom, &'static str> {
-    let mut process_stack: Vec<Atom> = vec![];
-    while !input_stack.is_empty() {
-        let atom = input_stack.pop().unwrap();
-
-        match atom {
-            Atom::Add => {
-                let rhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                let lhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                process_stack.push(Atom::Number(rhs + lhs));
-            },
-            Atom::Subtraction => {
-                let rhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                let lhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                process_stack.push(Atom::Number(rhs - lhs));
-            },
-            Atom::Muliplication => {
-                let rhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                let lhs = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-                process_stack.push(Atom::Number(rhs * lhs));
-            },
-            Atom::Division => {
-                let top = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-    
-
-                let bot = match process_stack.pop().unwrap() {
-                    Atom::Number(x) => Some(x),
-                    _ => None,
-                }.unwrap();
-
-                if bot == 0 {
-                    return Err("Error: Cannot divide by zero");
-                }
-                process_stack.push(Atom::Number(top / bot));
-            },
-            Atom::Number(x) => process_stack.push(Atom::Number(x)),
-            Atom::Literal(s) => process_stack.push(Atom::Literal(s)),
-        }
-    }
-
-    Ok(process_stack.pop().unwrap())
-}
-
 fn read_file(filename: String) -> std::io::Result<String>
 { let f = File::open(filename)?;
     let mut reader = BufReader::new(f);
@@ -248,24 +71,25 @@ fn read_file(filename: String) -> std::io::Result<String>
     Ok(data.trim().to_string())
 }
 
-fn shell_mode() -> std::io::Result<()> {
+pub fn shell_mode() -> std::io::Result<()> {
     let mut input = String::new();
     let prompt = "> ";
     print!("Starting Shell mode. Press Ctrl-C to exit.\n");
     loop {
         print!("{}", prompt);
-        io::stdout().flush()?;
-        let n = io::stdin().read_line(&mut input)?;
+        std::io::stdout().flush()?;
+        let n = std::io::stdin().read_line(&mut input)?;
         if n == 0 {
             break
         } else {
-            interpreter(input.clone());
+            penlisp::run(input.clone());
         }
         input.clear();
     }
 
     Ok(())
 }
+
 
 fn main() {
 
@@ -279,7 +103,7 @@ fn main() {
     if args.shell_mode {
         if !args.filename.is_empty() {
             let data = read_file(args.filename).expect("Failed to Read input file.");
-            interpreter(data);
+            penlisp::run(data);
         }
         match shell_mode() {
             Ok(_) => {
@@ -292,7 +116,7 @@ fn main() {
     } else {
         if !args.filename.is_empty() {
             let data = read_file(args.filename).expect("Failed to Read input file.");
-            interpreter(data);
+            penlisp::run(data);
         }
     }
 }
---
+++ src/penlisp.rs
@@ -0,0 +1,218 @@
+pub mod tokenizer {
+    use core::fmt;
+    use std::char;
+
+    #[derive(Debug, PartialEq)]
+    pub enum Kind {
+        Start,
+        Keyword,
+        Identifier,
+        Integer,
+        Decimal,
+        Literal,
+        End
+    }
+
+    impl fmt::Display for Kind {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Kind::Start => write!(f, "start"),
+                Kind::End => write!(f, "end"),
+                Kind::Keyword => write!(f, "keyword"),
+                Kind::Literal => write!(f, "literal"),
+                Kind::Integer => write!(f, "integer"),
+                Kind::Decimal => write!(f, "decimal"),
+                Kind::Identifier => write!(f, "identifier"),
+            }
+        }
+    }
+
+    static KEYWORDS: &[&'static str] = &[
+        "=", "+", "-", "*", "/", 
+        ">", ">=", "<", "<=", "!=",
+        "and", "or", "not", "true", 
+        "false", "if", "let", "defun", 
+        "lambda", "cons", "nil" 
+    ];
+
+    fn get_kind(token: &String) -> Kind {
+        let t = (*token).as_str();
+        match t {
+            "(" => Kind::Start,
+            ")" => Kind::End,
+            _ if KEYWORDS.contains(&t) => Kind::Keyword,
+            _ if t.starts_with("\"") && t.ends_with("\"")=> Kind::Literal,
+            _ if t.parse::<i64>().is_ok() => Kind::Integer,
+            _ if t.parse::<f64>().is_ok() => Kind::Decimal,
+            _ => {
+                let t_chars: Vec<char> = t.chars().collect();
+                if t_chars[0].is_ascii_alphabetic() || t_chars[0] == '_' {
+                    return Kind::Identifier;
+                }
+                panic!("Cannot parse token {}", t);
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct Token {
+        pub loc: usize,
+        pub kind: Kind,
+        pub value: String,
+    }
+
+    impl PartialEq<Kind> for Token {
+        fn eq(&self, other: &Kind) -> bool {
+            self.kind == *other
+        }
+    }
+
+    impl PartialEq<Token> for Kind {
+        fn eq(&self, other: &Token) -> bool {
+            *self == other.kind
+        }
+    }
+
+    impl fmt::Display for Token {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "Token{{loc: {}, kind: {}, value: {} }}", self.loc, self.kind, self.value)
+        }
+    }
+
+    pub fn tokenize(data: String) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+        let chars: Vec<char> = data.chars().collect();
+        let mut chars_index = 0;
+        let mut token = String::new();
+        while chars_index < chars.len() {
+            match chars[chars_index] {
+                ' ' | '\n' | '\t' => {
+                    if !token.is_empty() {
+                        tokens.push(Token{
+                            loc: chars_index - token.len(),
+                            kind: get_kind(&token),
+                            value: token.clone()
+                        });
+                        token.clear();
+                    }
+                }
+                '(' => {
+                    tokens.push(Token{
+                        loc: chars_index,
+                        kind: Kind::Start,
+                        value: String::from("("),
+                    })
+                }
+                ')' => {
+                    if !token.is_empty() {
+                        tokens.push(Token{
+                            loc: chars_index - token.len(),
+                            kind: get_kind(&token),
+                            value: token.clone()
+                        });
+                        token.clear();
+                    }
+                    tokens.push(Token{
+                        loc: chars_index,
+                        kind: Kind::End,
+                        value: String::from(")"),
+                    })
+                }
+                _ => {
+                    token.push(chars[chars_index]);
+                }
+            }
+
+            chars_index += 1;
+        } tokens
+    } 
+}
+
+
+pub fn run(data: String) {
+    let tokens: Vec<tokenizer::Token> = tokenizer::tokenize(data);
+    for token in tokens {
+        print!("{}\n", token);
+    }
+}
+
+// Graveyard:
+// if let Some(t) = tokens.pop() {
+//     match t.value.as_str() {
+//         "+" => {
+//             let lhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//             let rhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//
+//             result.push(Expression::Add(lhs, rhs));
+//         }
+//         "-" => {
+//             let lhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//             let rhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//
+//             result.push(Expression::Subtract(lhs, rhs));
+//         }
+//         "*" => {
+//             let lhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//             let rhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//
+//             result.push(Expression::Mulitply(lhs, rhs));
+//         }
+//         "/" => {
+//             let lhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//             let rhs = Box::new(Expression::Number(tokens.pop().unwrap().value.parse::<i64>().unwrap()));
+//
+//             result.push(Expression::Divide(lhs, rhs));
+//         }
+//         _ => unimplemented!("Blargg!!")
+//     }
+//
+// pub mod interpreter {
+//     use crate::penlisp::tokenizer;
+//
+//     #[derive(Debug)]
+//     pub enum Expression {
+//         Add(Box<Expression>, Box<Expression>),
+//         Subtract(Box<Expression>, Box<Expression>),
+//         Mulitply(Box<Expression>, Box<Expression>),
+//         Divide(Box<Expression>, Box<Expression>),
+//         Number(i64),
+//         // Decimal(f64),
+//         // Literal(String),
+//         Nil,
+//     }
+//
+//
+//     // (+ 1 (* 3 4))
+//     //      
+//     pub fn process(tokens: &mut Vec<tokenizer::Token>) -> Vec<Expression> {
+//         print!("{:?}\n", tokens);
+//         if tokens.len() == 0 {
+//             return vec![Expression::Nil];
+//         }
+//
+//         let mut result: Vec<Expression> = vec![];
+//         while tokens.len() > 0 {
+//             let t = match tokens.pop() {
+//                 Some(t) => t,
+//                 None => panic!("No tokens left!!"),
+//             };
+//
+//             match t.kind {
+//                 tokenizer::Kind::Start => {
+//                     let u = match tokens.pop() {
+//                         Some(u) => u,
+//                         None => panic!("No tokens left!!"),
+//                     };
+//
+//                     if u.kind != tokenizer::Kind::Keyword {
+//                         panic!("Expected Keyword after Start");
+//                     }
+//                 },
+//                 tokenizer::Kind::End => {},
+//                 tokenizer::Kind::Keyword => {},
+//                 tokenizer::Kind::Value => {},
+//             }
+//         }
+//         result
+//     }
+// }
